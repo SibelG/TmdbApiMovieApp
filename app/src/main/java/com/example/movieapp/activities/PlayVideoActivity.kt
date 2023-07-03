64ecedfbdca1d73e518e50d6821a031ecac54767
Sibel GENÇTÜRK <46397935+SibelG@users.noreply.github.com>
2023-07-03 11:05:23 +0300

    youtube player view

--- app/src/main/java/com/example/movieapp/activities/PlayVideoActivity.kt
+++ app/src/main/java/com/example/movieapp/activities/PlayVideoActivity.kt
@@ -2,6 +2,9 @@ package com.example.movieapp.activities
 
 
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movieapp.R
 import com.example.movieapp.databinding.ActivityPlayVideoBinding
@@ -9,131 +12,79 @@ import com.example.movieapp.utils.Constants
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.PlayerView
 import fr.bmartel.youtubetv.YoutubeTvView
-
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class PlayVideoActivity : AppCompatActivity() {
 
     lateinit var _binding: ActivityPlayVideoBinding
     private val binding get() = _binding!!
     lateinit var mQuery: String
-    lateinit var playerView:PlayerView
-    var player: SimpleExoPlayer ?= null
+    lateinit var playerView: PlayerView
+    var player: SimpleExoPlayer? = null
     var playWhenReady = true
     var currentWindow: Int = 0
-    var playBackPosition : Long = 0
+    var playBackPosition: Long = 0
     lateinit var youtubeTvView: YoutubeTvView
 
+    // on the below line we are creating a variable
+    // for our youtube player view.
+    lateinit var youtubePlayerView: YouTubePlayerView
+
+    // on below line we are creating a
+    // string variable for our video id.
+    var videoID = "vG2PNdI8axo"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // on below line we are setting
+        // screen orientation to landscape
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        // on below line we are setting flags to
+        // change our activity to full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_play_video)
-        youtubeTvView = findViewById(R.id.player)
+
+        // on below line we are hiding our action bar
+        actionBar?.hide()
+
+        // on below line we are initializing
+        // our youtube player view with its id.
+        youtubePlayerView = findViewById(R.id.videoPlayer)
+
+        // on below line we are setting full
+        // screen for our youtube player view.
+        /*youtubePlayerView.enterFullScreen()
+        youtubePlayerView.toggleFullScreen()*/
+
+        // on below line we are getting observer
+        // for our youtube player view.
+        lifecycle.addObserver(youtubePlayerView)
+
         val receivedIntent = intent
         mQuery = receivedIntent.getStringExtra(Constants.VIDEO_URI).toString()
-        youtubeTvView.playVideo(mQuery)
-    }
 
-
-    /*override fun onStart() {
-        super.onStart()
-        if(Util.SDK_INT>=24){
-            initPlayer()
-        }
-    }
-
-    override fun onStop() {
-        if(Util.SDK_INT < 24){
-            releasePlayer()
-        }
-        super.onStop()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        if(Util.SDK_INT < 24 || player == null ){
-            initPlayer()
-            hideSystemUI()
-        }
-    }
-
-    override fun onPause() {
-        if(Util.SDK_INT <24)
-            releasePlayer()
-        super.onPause()
-    }
-
-    private fun hideSystemUI() {
-        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE or
-        View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        _binding = ActivityPlayVideoBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        playerView = binding.videoView
-        val receivedIntent = intent
-        mQuery = receivedIntent.getStringExtra(Constants.VIDEO_URI).toString()
-        Log.d("mquery",mQuery)
-
-        if (mQuery == null || mQuery.trim().isEmpty()) finish()
-        initPlayer()
-    }
-
-    private fun initPlayer(){
-        player = SimpleExoPlayer.Builder(this).build()
-        playerView.player = player
-
-
-        playYoutubeVideo(mQuery)
-    }
-
-    private fun playYoutubeVideo(youtubeUrl: String?) {
-        @SuppressLint("StaticFieldLeak")
-        val mExtractor: YouTubeExtractor =
-            object : YouTubeExtractor(this) {
-                override fun onExtractionComplete(
-                    sparseArray: SparseArray<YtFile>?,
-                    videoMeta: VideoMeta?
-                ) {
-                    if (sparseArray != null) {
-                        var videoTag = 137
-                        var audioTag = 140
-                        var audioSource : MediaSource = ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory())
-                            .createMediaSource(MediaItem.fromUri(sparseArray[audioTag].url))
-
-                        var videoSource : MediaSource = ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory())
-                            .createMediaSource(MediaItem.fromUri(sparseArray[videoTag].url))
-
-                        player!!.setMediaSource(MergingMediaSource(true,
-                        videoSource,
-                        audioSource),
-                        true)
-
-                        player!!.prepare()
-                        player!!.playWhenReady=playWhenReady
-                        player!!.seekTo(currentWindow,playBackPosition)
-
-                    }
-                }
+        youtubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                // loading the selected video
+                // into the YouTube Player
+                youTubePlayer.loadVideo(mQuery, 0f)
             }
-        mExtractor.extract(youtubeUrl, true, true)
-    }
 
-    fun releasePlayer(){
-        if(player != null){
-            playWhenReady = player!!.playWhenReady
-            playBackPosition = player!!.currentPosition
-            currentWindow = player!!.currentWindowIndex
-            player!!.release()
-            player = null
-        }
+            override fun onStateChange(
+                youTubePlayer: YouTubePlayer,
+                state: PlayerConstants.PlayerState
+            ) {
+                // this method is called if video has ended,
+                super.onStateChange(youTubePlayer, state)
+            }
+        })
     }
-*/
 }
